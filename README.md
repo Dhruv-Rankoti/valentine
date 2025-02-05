ee2f7ba1073b8e3f366ac6dc4501b5e7b4b83f0b
Dhruv-Rankoti <dhruvrankoti@gmail.com>
2025-02-06 01:58:58 +0530

    adding files

---
+++ README.md
@@ -0,0 +1,3 @@
+# Valentine
+
+In a world where love often feels like a fleeting dream, this project seeks to bring a spark of hope amidst the impending doom of Valentine's Day. It's a heartfelt website that empowers users to muster the courage to ask their crush out on this special day, embracing the beauty of vulnerability, even if it leads to rejection in the end. After all, love is not just about the outcome, but the courage to express one's feelings.
---
+++ src/MyComponent.jsx
@@ -0,0 +1,55 @@
+import react, { useState } from "react";
+
+function MyComponent() {
+    const [img, setImg] = useState('manja');
+    const [text, setText] = useState('Will You be my Valentine? 🤗');
+    const [count, setCount] = useState(0);
+    const [showNoButton, setShowNoButton] = useState(true);
+
+    let imgUrl = `./src/assets/${img}.gif`;
+
+    function yes(e) {
+        setImg('kiss');
+        setText('Hehehehe, I knew it! 😘');
+        setCount(0);
+        setShowNoButton(false);
+    }
+    
+    function no(e) {
+        setCount(count => count + 1);
+        switch(count) {
+            case 0:
+                setImg('no1');
+                setText('Soch le ache se! 🙄');
+                break;
+            case 1:
+                setImg("no2");
+                setText('Ek aur baar soch le! 😣')
+                break;
+            default:
+                setImg("no3");
+                setText('Manja na! Kitna bhav khaegi 😭')
+        }
+        moveElement(e.currentTarget);
+    }
+
+    function moveElement(e) {
+        if (count <= 2) return;
+        e.style.position = "absolute";
+        e.style.top = `${Math.floor(Math.random() * 90 + 5)}%`;
+        e.style.left = `${Math.floor(Math.random() * 90 + 5)}%`;
+    }
+
+    return (
+        <div className="container">
+            <img className="my-image" src={imgUrl} alt="manja na" />
+            <h1>{text}</h1>
+            <div className="my-button">
+                {showNoButton && <button onClick={yes}>Yes</button>}
+                {showNoButton && <button onClick={no} onMouseEnter={(e) => moveElement(e.currentTarget)}>No</button>}
+            </div>
+        </div>
+    );
+}
+
+export default MyComponent;
